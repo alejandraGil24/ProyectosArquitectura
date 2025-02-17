a52cf2a6d8b3c220900285274a4864351da7921e
alejandraGil24 <alejandragilparedes380@gmail.com>
2025-02-18 00:14:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const lenis = new Lenis();
+  lenis.on("scroll", ScrollTrigger.update);
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 500);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+
+  const stickySection = document.querySelector(".sticky");
+  const slidesContainer = document.querySelector(".slides");
+  const slider = document.querySelector(".slider");
+  const slides = document.querySelectorAll(".slide");
+
+  const stickyHeight = window.innerHeight * 3;
+  const totalMove = slidesContainer.scrollWidth - slider.offsetWidth;
+  const slideWidth = slides[0].offsetWidth;
+
+  ScrollTrigger.create({
+    trigger: stickySection,
+    start: "top top",
+    end: `+=${stickyHeight}px`,
+    scrub: 1,
+    pin: true,
+    pinSpacing: true,
+    onUpdate: (self) => {
+      const progress = self.progress;
+      const mainMove = progress * totalMove;
+
+      gsap.set(slidesContainer, { x: -mainMove });
+
+      const currentSlide = Math.floor(mainMove / slideWidth);
+      const sliderProgress = (mainMove % slideWidth) / slideWidth;
+
+      slides.forEach((slide, index) => {
+        const image = slide.querySelector("img");
+
+        if (image) {
+          if (index === currentSlide || index === currentSlide + 1) {
+            const relativeProgress =
+              index === currentSlide
+                ? sliderProgress
+                : Math.max(sliderProgress - 1, 0);
+            const parallaxAmount = relativeProgress * slideWidth * 0.05;
+
+            gsap.set(image, {
+              x: parallaxAmount,
+              scale: 1,
+            });
+          } else {
+            gsap.set(image, {
+              x: 0,
+              scale: 1,
+            });
+          }
+        }
+      });
+    },
+  });
+});
